5b3413109c8c158627b17b137627cf3ced8c2456
Guillaume <deroguerre@gmail.com>
2017-04-02 21:02:36 +0200

    mocha test; add player + boat class; split js code

---
+++ main.js
@@ -0,0 +1,106 @@
+	//var grid = new Array();
+
+	/* MAIN SCRIPT */
+
+	function test() {
+		return 10;
+	}
+
+	function refreshScreen() {
+
+		console.clear();
+		console.log("JOUEUR 1");
+		oGridPlayer1.diplayGrid();
+		console.log("JOUEUR 2");
+		oGridPlayer2.diplayGrid();
+	}
+
+    class Player {
+
+        constructor(name) {
+            this.name = name;
+            this.listBoat = new Array();
+        }
+
+		attack(x,y,numGrid) {
+
+		}
+
+    }
+
+	class Grid {
+
+		constructor(gridSize) {
+			
+			this.gridSize = gridSize;
+			this.grid = new Array();
+
+			for(var i = 0; i < gridSize; i++) {
+				this.grid[i] = new Array();
+				for(var j = 0; j < gridSize; j++) {
+					this.grid[i][j] = 0;
+				}
+			}
+		}
+
+		diplayGrid() {
+            
+			$.each(this.grid, function(key, value) {
+				console.log(value);
+			});
+		}
+	}
+
+	let oGridPlayer1 = new Grid(5);
+	let oGridPlayer2 = new Grid(5);
+
+	class Boat {
+
+		constructor(playerId,startX,startY, width, lenght) {
+			this.playerId = playerId;
+			this.startX = startX;
+			this.startY = startY;
+			this.width = width;
+			this.lenght = lenght;
+		}
+
+		setBoat() {
+
+			for(var i = 0; i < this.width; i++){
+				for(var j = 0; j < this.lenght; j++) {
+
+					oGridPlayer1.grid[this.startX+j][this.startY+i] = 1;
+				}
+			}
+
+			refreshScreen();
+		}
+	}
+
+	refreshScreen();
+
+    var oPlayer1 = new Player();
+	var boat1J1 = new Boat(1,1,1,2,2,0,5);
+    
+    oPlayer1.listBoat.push(boat1J1);
+
+	boat1J1.setBoat();
+
+	function askAttackPosition() {
+
+		try {
+			var attackPosition = prompt("Saisir une position d'attaque (x,y)", "x,y");
+			var attackPosition = attackPosition.split(",");
+			console.log(attackPosition);
+			return attackPosition;
+		} catch(err) {
+			return false;
+		}
+
+	}
+	
+	askAttackPosition();
+
+	//var boat1 = new boat(2,2,"hor",1,1,2);
+
+	/* END MAIN SCRIPT */
---
+++ tests.js
@@ -0,0 +1,73 @@
+
+/* MOCHA TEST */
+
+mocha.setup('bdd');
+
+var expect = chai.expect;
+
+/*describe('Teste de la function test', function() {
+    it('really runs', function() {
+    expect(test()).to.equal(9);
+    });
+});*/
+
+describe("Test de la classe 'Grid'", function() {
+
+    let oGrid;
+
+    beforeEach(() => {
+        oGrid = new Grid();
+    })
+
+    it("La grille est un tableau", function() {
+        chai.assert.isArray(oGrid.grid);
+    })
+
+    it("La grille n'est pas null", function() {
+        expect(oGrid.grid).to.not.be.null;
+    })
+
+
+})
+
+describe("Test sur la classe 'Boat'", function() {
+
+    beforeEach(() => {
+        let testBoat = new Boat(1,1,1,2,2,0,5);
+    })
+
+    /*it("Le bateau n'est pas en dehors de la grille", function() {
+        chai.assert.isOk('everything', 'everything is ok');
+    })*/
+})
+
+describe("Test sur la classe 'Player'", function() {
+
+    let oPlayer;
+
+    beforeEach(() => {
+        oPlayer = new Player();
+    })
+
+    it("oPlayer est un objet", function() {
+        //chai.assert.ifError(oPlayer.attack(-1,-1,0))('everything', 'everything is ok');
+        chai.assert.typeOf(oPlayer, 'object');
+
+    })
+})
+
+describe("Test la fonction d'attaque", function() {
+
+    /*it("reponse envoyé", function() {
+        chai.assert.isOk(askAttackPosition());
+    })
+
+    it("retourne un tableau de position", function() {
+        chai.assert.isArray(askAttackPosition());
+    })*/
+
+})
+
+mocha.run();
+
+/* END MOCHA */
